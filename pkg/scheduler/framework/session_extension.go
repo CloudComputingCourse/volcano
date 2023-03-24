009b550e1e2b1a88f01756f4253bcf64782aa3fe
Tianya Chen <tianyakc@gmail.com>
2023-03-23 20:05:14 -0400

    fixed the glog type, should be klog in session_extension.go

--- pkg/scheduler/framework/session_extension.go
+++ pkg/scheduler/framework/session_extension.go
@@ -10,7 +10,7 @@ func (ssn Session) GetPolicy(schedulerConf string) string {
 	var err error
 	conf := map[string]string{"policy": "fifoRandom"}
 	if conf, err = ssn.cache.LoadSchedulerConf(schedulerConf); err != nil {
-		glog.Errorf("Failed to load scheduler policy '%s', using default fifoRandom policy: %v",
+		klog.Errorf("Failed to load scheduler policy '%s', using default fifoRandom policy: %v",
 			schedulerConf, err)
 	}
 
@@ -24,6 +24,6 @@ func (ssn Session) GetPolicy(schedulerConf string) string {
 
 func (ssn Session) UpdateScheduledTime(task *api.TaskInfo) {
 	if err := ssn.cache.UpdateScheduledTime(task); err != nil {
-		glog.Errorf("Failed to update scheduled time of task <%v/%v>: %v", task.Namespace, task.Name, err)
+		klog.Errorf("Failed to update scheduled time of task <%v/%v>: %v", task.Namespace, task.Name, err)
 	}
 }
