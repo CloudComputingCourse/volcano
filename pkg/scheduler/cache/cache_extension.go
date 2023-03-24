cac1fcadda2d388a7383b0386be721312e0c9c33
Tianya Chen <tianyakc@gmail.com>
2023-03-23 23:06:03 -0400

    added the empty context to podgetter and configgetter, added the updateoptions to pods.update

--- pkg/scheduler/cache/cache_extension.go
+++ pkg/scheduler/cache/cache_extension.go
@@ -3,6 +3,7 @@ package cache
 import (
 	"fmt"
 	"time"
+	"context"
 
 	"k8s.io/klog/v2"
 
@@ -15,13 +16,13 @@ import (
 
 func (sc *SchedulerCache) UpdateScheduledTime(task *volapi.TaskInfo) error {
 	sc.Mutex.Lock()
-        defer sc.Mutex.Unlock()
+    defer sc.Mutex.Unlock()
 
 	updateInterval := time.Second
 	retryTimes := 5
 	scheduledTime := metav1.NewTime(time.Now())
 	for i :=0; i < retryTimes; i++ {
-		pod, err := sc.kubeClient.CoreV1().Pods(task.Pod.Namespace).Get(task.Pod.Name, metav1.GetOptions{})
+		pod, err := sc.kubeClient.CoreV1().Pods(task.Pod.Namespace).Get(context.TODO(), task.Pod.Name, metav1.GetOptions{})
 		if err != nil {
 			if apierrors.IsNotFound(err) {
 				return nil
@@ -34,7 +35,7 @@ func (sc *SchedulerCache) UpdateScheduledTime(task *volapi.TaskInfo) error {
 		}
 		pod.ObjectMeta.Annotations["scheduledTime"] = scheduledTime.Rfc3339Copy().String()
 
-		pod, err = sc.kubeClient.CoreV1().Pods(pod.Namespace).Update(pod)
+		pod, err = sc.kubeClient.CoreV1().Pods(pod.Namespace).Update(context.TODO(), pod, metav1.UpdateOptions{})
 		if err == nil {
 			return nil
 		}
@@ -56,7 +57,7 @@ func (sc *SchedulerCache) LoadSchedulerConf(path string) (map[string]string, err
 		return nil, err
 	}
 
-	confMap, err := sc.kubeClient.CoreV1().ConfigMaps(ns).Get(name, metav1.GetOptions{})
+	confMap, err := sc.kubeClient.CoreV1().ConfigMaps(ns).Get(context.TODO(), name, metav1.GetOptions{})
 	if err != nil {
 		return nil, err
 	}
