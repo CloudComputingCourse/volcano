a98276c95093f2e188b2c377990425e2d9f6f6d8
Tianya Chen <tianyakc@gmail.com>
2023-03-23 20:48:53 -0400

    added the cache_extension.go

---
+++ pkg/scheduler/cache/cache_extension.go
@@ -0,0 +1,65 @@
+package cache
+
+import (
+	"fmt"
+	"time"
+
+	"k8s.io/klog/v2"
+
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/tools/cache"
+
+	volapi 	"volcano.sh/volcano/pkg/scheduler/api"
+)
+
+func (sc *SchedulerCache) UpdateScheduledTime(task *volapi.TaskInfo) error {
+	sc.Mutex.Lock()
+        defer sc.Mutex.Unlock()
+
+	updateInterval := time.Second
+	retryTimes := 5
+	scheduledTime := metav1.NewTime(time.Now())
+	for i :=0; i < retryTimes; i++ {
+		pod, err := sc.kubeclient.CoreV1().Pods(task.Pod.Namespace).Get(task.Pod.Name, metav1.GetOptions{})
+		if err != nil {
+			if apierrors.IsNotFound(err) {
+				return nil
+			}
+			return err
+		}
+
+		if pod.ObjectMeta.Annotations == nil {
+			pod.ObjectMeta.Annotations = make(map[string]string)
+		}
+		pod.ObjectMeta.Annotations["scheduledTime"] = scheduledTime.Rfc3339Copy().String()
+
+		pod, err = sc.kubeclient.CoreV1().Pods(pod.Namespace).Update(pod)
+		if err == nil {
+			return nil
+		}
+		if err != nil && !apierrors.IsConflict(err) {
+			if apierrors.IsNotFound(err) {
+				return nil
+			}
+			glog.Errorf("falied to update pod scheduled time: %v", err)
+			return err
+		}
+		time.Sleep(updateInterval)
+	}
+	return fmt.Errorf("update pod schuduled time failed after %d retries", retryTimes)
+}
+
+func (sc *SchedulerCache) LoadSchedulerConf(path string) (map[string]string, error) {
+	ns, name, err := cache.SplitMetaNamespaceKey(path)
+	if err != nil {
+		return nil, err
+	}
+
+	confMap, err := sc.kubeclient.CoreV1().ConfigMaps(ns).Get(name, metav1.GetOptions{})
+	if err != nil {
+		return nil, err
+	}
+
+	return confMap.Data, nil
+}
