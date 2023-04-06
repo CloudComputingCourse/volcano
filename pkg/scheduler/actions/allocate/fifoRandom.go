df2ec6526f67496476923ab28d1bf9f48aadc1d7
Baljit Singh <baljit92@gmail.com>
2023-04-06 03:50:27 +0000

    dummy commit

--- pkg/scheduler/actions/allocate/fifoRandom.go
+++ pkg/scheduler/actions/allocate/fifoRandom.go
@@ -2,23 +2,42 @@ package allocate
 
 import (
 	"volcano.sh/volcano/pkg/scheduler/api"
+	"math/rand"
+	"time"
 )
 
 func fifoRandomFn(jobs []*api.JobInfo, nodes []*api.NodeInfo) map[*api.TaskInfo]*api.NodeInfo {
+	// TODO: UPDATE THIS FUNCTION
 	allocation := make(map[*api.TaskInfo]*api.NodeInfo)
-	jobAlloc := make(map[*api.TaskInfo]*api.NodeInfo)
+
 	if len(jobs) == 0 {
 		return allocation
 	}
 
 	job := jobs[0]
-
-	if allocateFallback(job, nodes, jobAlloc) {
-
-		for t, n := range jobAlloc {
-			allocation[t] = n
+	// get available nodes
+	availableNodeIdx := []int{}
+	for i, node := range nodes {
+		if len(node.Tasks) == 0 {
+			availableNodeIdx = append(availableNodeIdx, i)
 		}
 	}
-
+	// shuffle the nodes index
+	rand.Seed(time.Now().UnixNano())
+	rand.Shuffle(len(availableNodeIdx), func(i, j int) {availableNodeIdx[i], availableNodeIdx[j] = availableNodeIdx[j], availableNodeIdx[i]})
+	// allocate
+	i := 0
+	for _, task := range job.TaskStatusIndex[api.Pending] {
+		if i < len(availableNodeIdx) {
+			allocation[task] = nodes[availableNodeIdx[i]]
+		} else {
+			break
+		}
+		i++
+	}
+	if len(job.TaskStatusIndex[api.Pending]) != len(allocation) {
+		// could not allocate all the tasks, return empty allocation
+		allocation = make(map[*api.TaskInfo]*api.NodeInfo)
+	}
 	return allocation
 }
--- pkg/scheduler/actions/allocate/sjfHeter.go
+++ pkg/scheduler/actions/allocate/sjfHeter.go
@@ -1,46 +1,30 @@
 package allocate
 
 import (
-	"math"
-	"strconv"
-
 	"volcano.sh/volcano/pkg/scheduler/api"
+	"math/rand"
 )
 
 func sjfHeterFn(jobs []*api.JobInfo, nodes []*api.NodeInfo) map[*api.TaskInfo]*api.NodeInfo {
-
+	// TODO: UPDATE THIS FUNCTION
 	allocation := make(map[*api.TaskInfo]*api.NodeInfo)
-
-	shortestTime := math.MaxInt32
-
-	var resMachines map[*api.TaskInfo]*api.NodeInfo
-	for _, job := range jobs {
-
-		task := getOneTask(job)
-		jobType := task.Pod.ObjectMeta.Labels["type"]
-		fastDuration, _ := strconv.Atoi(task.Pod.ObjectMeta.Labels["FastDuration"])
-		slowDuration, _ := strconv.Atoi(task.Pod.ObjectMeta.Labels["SlowDuration"])
-		freeNode := getFreeNode(nodes, task, jobType)
-		jobAlloc := make(map[*api.TaskInfo]*api.NodeInfo)
-		if allocateJob(job, freeNode, jobAlloc, jobType) && fastDuration < shortestTime {
-			shortestTime = fastDuration
-			resMachines = make(map[*api.TaskInfo]*api.NodeInfo)
-			for t, n := range jobAlloc {
-				resMachines[t] = n
-			}
-
-		} else if allocateFallback(job, nodes, jobAlloc) && slowDuration < shortestTime {
-			shortestTime = slowDuration
-			resMachines = make(map[*api.TaskInfo]*api.NodeInfo)
-			for t, n := range jobAlloc {
-				resMachines[t] = n
-			}
+	i := 0
+	job := jobs[rand.Intn(len(jobs))]
+	for _, task := range job.TaskStatusIndex[api.Pending] {
+		for i<len(nodes) && len(nodes[i].Tasks) > 0 {
+			// skip nodes that have tasks running
+			i++
 		}
+		if i>=len(nodes) {
+			// out of nodes
+			break
+		}
+		allocation[task] = nodes[i]
+		i++
 	}
-
-	for t, n := range resMachines {
-		allocation[t] = n
+	if len(job.TaskStatusIndex[api.Pending]) != len(allocation) {
+		// could not allocate all the tasks, return empty allocation
+		allocation = make(map[*api.TaskInfo]*api.NodeInfo)
 	}
-
 	return allocation
 }
